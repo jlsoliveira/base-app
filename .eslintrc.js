1c25b24ca9ca804d92857ad4e88ff15b987290b1
Hanna Liz <hannaliz@MacBook-Pro-de-Hanna.local>
2025-09-04 18:09:16 -0300

    feat: husky ajusted

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,8 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "prettier"
   ],
-  "plugins": ["prettier", "@typescript-eslint"],
+  "plugins": ["@typescript-eslint"],
   "rules": {
-    "prettier/prettier": "error",
     "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }]
   }
 };
--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
 import { useAppTheme } from '@hooks';
 import React from 'react';
-import { StatusBar, StyleSheet } from 'react-native';
+import { StatusBar } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
 
 export default function App() {
 
   const theme = useAppTheme();
   return (
-    <PaperProvider theme={themer}>
+    <PaperProvider theme={theme}>
       <StatusBar barStyle={theme.dark ? 'light-content' : 'dark-content'} />
     </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
